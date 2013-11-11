864a5c39412dd2657cb7672235b803d6e53fa062
hugolopez <hugo.lopez@ulsa.mx>
2013-11-11 13:46:38 +0000

    conclusion de ejercicio01

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
-class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+class VisitorsController < ApplicationController
+  def new
+    @owner = Owner.new
+    render 'visitors/new'
+  end
 end
